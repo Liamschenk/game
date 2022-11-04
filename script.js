1ace0454e0187e324ce79d4fdca0242e57f7785c
Liam Schenk <89917505+Liamschenk@users.noreply.github.com>
2022-11-04 11:46:46 +0100

    version with shot
    shot doesn't work but is in code

--- script.js
+++ script.js
@@ -1,64 +1,75 @@
+var playground = document.querySelector('.playground')
 var player = document.querySelector('.player')
-var displayPoints = document.querySelector('.punkte')
+var enemies = document.querySelectorAll('.enemy')
+var shots = document.querySelectorAll('.shot')
+// var timer = new Timer(5)
+// var enemy = document.querySelector('.enemy')
 
-var enemy1 = document.querySelector('.enemy1')
-var enemy2 = document.querySelector('.enemy2')
-var music = new Audio('sounds/theme.mp3')
-var augh = new Audio('sounds/kill.mp3')
-var score = 0
-
-
-player.style.left = '0px'
 player.style.top = '0px'
 
-// function toggleNav() {
-//   var menuscreen = document.querySelector('.menuscreen')
-//   if (menuscreen.style.display === "none") {
-//     menuscreen.style.display = "block";
-//   } else {
-//     menuscreen.style.display = "none";
-// }
-
 function loop() {
-  music.play(loop)
-
-  if(keyboard(68)) {
-    player.style.left = parseInt(player.style.left) + 5 + 'px'
-  }
-  if(keyboard(65)) {
-    player.style.left = parseInt(player.style.left) - 5 + 'px'
-  }
   if(keyboard(83)) {
     player.style.top = parseInt(player.style.top) + 5 + 'px'
   }
   if(keyboard(87)) {
     player.style.top = parseInt(player.style.top) - 5 + 'px'
   }
-
   if(keyboard(16)) {
-    if(keyboard(68)) {
-      player.style.left = parseInt(player.style.left) + 10 + 'px'
-    }
-    if(keyboard(65)) {
-      player.style.left = parseInt(player.style.left) - 10 + 'px'
-    }
     if(keyboard(83)) {
-      player.style.top = parseInt(player.style.top) + 10 + 'px'
+      player.style.top = parseInt(player.style.top) + 8 + 'px'
     }
     if(keyboard(87)) {
-      player.style.top = parseInt(player.style.top) - 10 + 'px'
+      player.style.top = parseInt(player.style.top) - 8 + 'px'
+    }
+    else{
+
     }
   }
 
-  // Kommentar: sobald der Spieler mit Gegner3 oder 4 kollidiert, werden diese gelöscht
-  var collisions = allCollisions(player, [enemy1, enemy2])
-  // Kommentar: wir gehen durch alle Kollisionsobjekte durch und löschen sie
-  for(var collision of collisions) {
-    collision.parentNode.removeChild(collision)
-    augh.play()
-    score = score + 1
-    displayPoints.textContent = score
+  // if(timer.ready()) {
+  //   var h = document.createElement('div')
+  //   h.classList.add('enemy')
+  //   h.style.top = '0px'
+  //   h.style.left = '100px'
+  //   playground.appendChild(h)
+  // }
+
+  for(var enemy of enemies) {
+    enemy.style.top = parseInt(enemy.style.top) + 5 + 'px'
+    if(parseInt(enemy.style.top) > 1000) {
+      enemy.parentNode.removeChild(enemy)
+    }
   }
+
+  if(mouseClick()) {
+    var playerX = parseInt(player.style.left)
+    var playerY = parseInt(player.style.top)
+    var a = angle(playerX, playerY, mousePositionX(playground), mousePositionY(playground))
+
+    var shot = document.createElement('div')
+    shot.classList.add('shot')
+    shot.style.left = player.style.left
+    shot.style.top = player.style.top
+    shot.setAttribute('data-angle', (180 - a) * Math.PI / 180)
+    playground.appendChild(shot)
+  }
+
+  for(var shot of shots) {
+  	var xPos = parseFloat(shot.style.left)
+    var yPos = parseFloat(shot.style.top)
+    var rotation = shot.getAttribute('data-angle')
+    shot.style.left = 3 * Math.sin(rotation) + xPos + 'px'
+    shot.style.top = 3 * Math.cos(rotation) + yPos + 'px'
+    if(parseInt(shot.style.left) < 0 || parseInt(shot.style.left) > 400 ||
+        parseInt(shot.style.top) < 0 || parseInt(shot.style.top) > 400) {
+    	shot.parentNode.removeChild(shot)
+    }
+  }
+
+  // if(anyCollision(player, [enemy])) {
+  //   alert("Game over!")
+  //   return
+  // }
   window.requestAnimationFrame(loop)
 }
 
