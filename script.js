ba5af25683e33a11c2d7f0f117e16043c4cff780
liamheininja <liamnils.heiniger@bbz-cfp.ch>
2022-11-25 11:11:25 +0100

    push

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var playground = document.querySelector('.playground')
 var player = document.querySelector('.player')
 var enemies = document.querySelectorAll('.enemy')
 var shots = document.querySelectorAll('.shot')
+var timer = new Timer(30)
 // var timer = new Timer(5)
 // var enemy = document.querySelector('.enemy')
 
@@ -29,20 +30,28 @@ function movement(){
 }
 
 function loop() {
+  if (keyboard(32)) {
+    var h = document.createElement('div')
+    h.classList.add('stone')
+    h.style.top = '0px'
+    h.style.left = '100px'
+    playground.appendChild(h)
+  }
+
+  var stonee = document.querySelectorAll('.stone')
+  for(var stone of stonee) {
+    stone.style.top = parseInt(stone.style.top) + 5 + 'px'
+    if(parseInt(stone.style.top) > 400) {
+      stone.parentNode.removeChild(stone)
+    }
+  }
+
   // Movement with dash on shift
   movement()
 
   //Shot on space press
 
   //Collisions shot/enemy
-
-  window.requestAnimationFrame(loop)
-}
-
-window.requestAnimationFrame(loop)
-
-
-function loop() {
 
   window.requestAnimationFrame(loop)
 }
