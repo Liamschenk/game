1bafc1be601339c50557029e16870212f95b64ca
Liam Schenk <89917505+Liamschenk@users.noreply.github.com>
2022-11-25 11:07:37 +0100

    prettier + score display

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-var playground = document.querySelector('.playground')
-var player = document.querySelector('.player')
-var enemies = document.querySelectorAll('.enemy')
-var shots = document.querySelectorAll('.shot')
-// var timer = new Timer(5)
-// var enemy = document.querySelector('.enemy')
+var playground = document.querySelector(".playground");
+var player = document.querySelector(".player");
+var enemies = document.querySelectorAll(".enemy");
+var shots = document.querySelectorAll(".shot");
 
-player.style.top = '0px'
-player.style.left = '0px'
+var displayPoints = document.querySelector(".score");
+var score = 0;
 
-function movement(){
-  if(keyboard(83)) {
-    player.style.top = parseInt(player.style.top) + 5 + 'px'
+player.style.top = "0px";
+player.style.left = "0px";
+
+function movement() {
+  if (keyboard(83)) {
+    player.style.top = parseInt(player.style.top) + 5 + "px";
   }
-  if(keyboard(87)) {
-    player.style.top = parseInt(player.style.top) - 5 + 'px'
+  if (keyboard(87)) {
+    player.style.top = parseInt(player.style.top) - 5 + "px";
   }
-  if(keyboard(16)) {
-    if(keyboard(83)) {
-      player.style.top = parseInt(player.style.top) + 8 + 'px'
+  if (keyboard(16)) {
+    if (keyboard(83)) {
+      player.style.top = parseInt(player.style.top) + 8 + "px";
     }
-    if(keyboard(87)) {
-      player.style.top = parseInt(player.style.top) - 8 + 'px'
+    if (keyboard(87)) {
+      player.style.top = parseInt(player.style.top) - 8 + "px";
+    } else {
     }
-    else{
+  }
+}
 
-    }
+function scoreDisplay() {
+  if () {
+    score = score + 1;
+    displayPoints.textContent = score;
+  } else {
   }
 }
 
 function loop() {
   // Movement with dash on shift
-  movement()
-
+  movement();
+  //Score
+  // scoreDisplay();
   //Shot on space press
 
   //Collisions shot/enemy
 
-  window.requestAnimationFrame(loop)
+  window.requestAnimationFrame(loop);
 }
 
-window.requestAnimationFrame(loop)
-
-
-function loop() {
-
-  window.requestAnimationFrame(loop)
-}
-
-window.requestAnimationFrame(loop)
+window.requestAnimationFrame(loop);
