917c3ec8d7e7fde7b0cef740d125128e7a040c21
Liam Schenk <89917505+Liamschenk@users.noreply.github.com>
2022-12-16 09:14:24 +0100

    revert
    revert to old enemy generation

--- script.js
+++ script.js
@@ -3,28 +3,15 @@ var playground = document.querySelector(".playground");
 var player = document.querySelector(".player");
 var shot = document.querySelector(".shot");
 
-//Testenemy
-// var enemy1 = document.querySelector('.enemy1')
-// var enemy2 = document.querySelector('.enemy2')
-// var enemy3 = document.querySelector('.enemy3')
-// var enemy4 = document.querySelector('.enemy4')
-// var enemy5 = document.querySelector('.enemy5')
-
 //Sounds
 var theme = new Audio("assets/sounds/theme.ogg");
 var dash = new Audio("assets/sounds/dash.wav");
 var shuriken = new Audio("assets/sounds/shuriken.wav");
 var gameOver = new Audio("assets/sounds/gameover.wav");
 
-//Timers for enemy spawn
-var enemytimer1 = new Timer(350);
-var enemytimer2 = new Timer(430);
-var enemytimer3 = new Timer(496);
-var enemytimer4 = new Timer(410);
-var enemytimer5 = new Timer(370);
-
 //Timer for shot delay
 var shottimer = new Timer(40);
+var enemytimer = new Timer(140);
 
 //Points display
 var displayPoints = document.querySelector(".score");
@@ -37,18 +24,18 @@ player.style.left = "40px";
 //Movement function
 function movement() {
   if (keyboard(83)) {
-    player.style.top = parseInt(player.style.top) + 3 + "px";
+    player.style.top = parseInt(player.style.top) + 6 + "px";
   }
   if (keyboard(87)) {
-    player.style.top = parseInt(player.style.top) - 3 + "px";
+    player.style.top = parseInt(player.style.top) - 6 + "px";
   }
   if (keyboard(16)) {
     if (keyboard(83)) {
-      player.style.top = parseInt(player.style.top) + 6 + "px";
+      player.style.top = parseInt(player.style.top) + 8 + "px";
       dash.play();
     }
     if (keyboard(87)) {
-      player.style.top = parseInt(player.style.top) - 6 + "px";
+      player.style.top = parseInt(player.style.top) - 8 + "px";
       dash.play();
     }
   }
@@ -63,12 +50,11 @@ function bullet() {
     h.style.left = 64 + "px";
     playground.appendChild(h);
     shuriken.play();
-    shottimer = new Timer(40);
   }
 
   var shots = document.querySelectorAll(".shot");
   for (var shot of shots) {
-    shot.style.left = parseInt(shot.style.left) + 8 + "px";
+    shot.style.left = parseInt(shot.style.left) + 10 + "px";
     if (parseInt(shot.style.left) > 720) {
       shot.parentNode.removeChild(shot);
     }
@@ -76,100 +62,20 @@ function bullet() {
 }
 
 //Enemy functions
-function enemy1() {
-  if (enemytimer1.ready()) {
-    var random = Math.floor(Math.random() * (124 - 40) + 40);
+function enemy() {
+  if (enemytimer.ready()) {
+    var random = Math.floor(Math.random() * (720 - 40) + 40);
     var h = document.createElement("div");
-    h.classList.add("enemy1");
+    h.classList.add("enemy");
     h.style.top = random + "px";
     h.style.right = "40px";
     playground.appendChild(h);
   }
 
-  var enemies = document.querySelectorAll(".enemy1");
+  var enemies = document.querySelectorAll(".enemy");
 
   for (var enemy of enemies) {
-    enemy.style.right = parseInt(enemy.style.right) + 1 + "px";
-    if (parseInt(enemy.style.right) > 720) {
-      enemy.parentNode.removeChild(enemy);
-    }
-  }
-}
-
-function enemy2() {
-  if (enemytimer2.ready()) {
-    var random = Math.floor(Math.random() * (268 - 164) + 164);
-    var h = document.createElement("div");
-    h.classList.add("enemy2");
-    h.style.top = random + "px";
-    h.style.right = "40px";
-    playground.appendChild(h);
-  }
-
-  var enemies = document.querySelectorAll(".enemy2");
-
-  for (var enemy of enemies) {
-    enemy.style.right = parseInt(enemy.style.right) + 1 + "px";
-    if (parseInt(enemy.style.right) > 720) {
-      enemy.parentNode.removeChild(enemy);
-    }
-  }
-}
-
-function enemy3() {
-  if (enemytimer3.ready()) {
-    var random = Math.floor(Math.random() * (412 - 308) + 308);
-    var h = document.createElement("div");
-    h.classList.add("enemy3");
-    h.style.top = random + "px";
-    h.style.right = "40px";
-    playground.appendChild(h);
-  }
-
-  var enemies = document.querySelectorAll(".enemy3");
-
-  for (var enemy of enemies) {
-    enemy.style.right = parseInt(enemy.style.right) + 1 + "px";
-    if (parseInt(enemy.style.right) > 720) {
-      enemy.parentNode.removeChild(enemy);
-    }
-  }
-}
-
-function enemy4() {
-  if (enemytimer4.ready()) {
-    var random = Math.floor(Math.random() * (556 - 452) + 452);
-    var h = document.createElement("div");
-    h.classList.add("enemy4");
-    h.style.top = random + "px";
-    h.style.right = "40px";
-    playground.appendChild(h);
-  }
-
-  var enemies = document.querySelectorAll(".enemy4");
-
-  for (var enemy of enemies) {
-    enemy.style.right = parseInt(enemy.style.right) + 1 + "px";
-    if (parseInt(enemy.style.right) > 720) {
-      enemy.parentNode.removeChild(enemy);
-    }
-  }
-}
-
-function enemy5() {
-  if (enemytimer5.ready()) {
-    var random = Math.floor(Math.random() * (720 - 596) + 596);
-    var h = document.createElement("div");
-    h.classList.add("enemy5");
-    h.style.top = random + "px";
-    h.style.right = "40px";
-    playground.appendChild(h);
-  }
-
-  var enemies = document.querySelectorAll(".enemy5");
-
-  for (var enemy of enemies) {
-    enemy.style.right = parseInt(enemy.style.right) + 1 + "px";
+    enemy.style.right = parseInt(enemy.style.right) + 2 + "px";
     if (parseInt(enemy.style.right) > 720) {
       enemy.parentNode.removeChild(enemy);
     }
@@ -191,11 +97,7 @@ function loop() {
   movement();
 
   //Enemy spawn functions
-  enemy1();
-  enemy2();
-  enemy3();
-  enemy4();
-  enemy5();
+  enemy();
 
   //Shot
   bullet();
